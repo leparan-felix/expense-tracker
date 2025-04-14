68e37b18ba976ffc28eaaef2a54f020b32c4cae4
leparan <leparanfelix16@gmail.com>
2025-04-14 20:55:21 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/expense-tracker/', // <-- set this to your repo name
   plugins: [react()],
-})
+  base: '/expense-tracker/', // 👈 Add or update this line
+});
+
